9e954a56b8a622e6a13dcf8c2f0b1815772771a2
qwqc <lz@live.de>
2025-06-22 22:57:32 +0200

    Initial commit

---
+++ BlackModule.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia.Controls;
+using Dockly.ModuleContracts;
+
+namespace Dockly.Modules
+{
+    public partial class BlackModule : UserControl, IModule
+    {
+        // Identification
+        public string Id => "DefaultModule";
+        public string ModuleName => "Default Module";
+        public string ModuleVersion => "1.0.0";
+        public string Category => "Default";
+        public string[] Tags => new string[] { "Default", "example" };
+
+        // Layout info
+        public int TileWidth => 2;
+        public int TileHeight => 2;
+
+        // Compatibility
+        public string MinAppVersion => "1.0.0";
+        public string MaxAppVersion => "2.0.0";
+        public string[] SupportedPlatforms => new string[] { "Windows", "Linux", "Mac" };
+
+        public BlackModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
